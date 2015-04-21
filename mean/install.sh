ce1b540f72c7b7e3418ee869ab4bf3392e94c3a6
Andreas Joelsson <andreas.joelsson@gmail.com>
2015-04-21 20:55:17 +0200

    Manual steps for npm installs

--- mean/install.sh
+++ mean/install.sh
@@ -15,31 +15,33 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Add mongo db
-sudo touch /etc/yum.repos.d/mongodb.repo
-sudo sh -c 'echo "[mongodb]" > /etc/yum.repos.d/mongodb.repo'
-sudo sh -c 'echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb.repo'
-sudo sh -c 'echo "baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/" >> /etc/yum.repos.d/mongodb.repo'
-sudo sh -c 'echo "gpgcheck=0" >> /etc/yum.repos.d/mongodb.repo'
-sudo sh -c 'echo "enabled=1" >> /etc/yum.repos.d/mongodb.repo'
-sudo sh -c 'echo "" >> /etc/yum.repos.d/mongodb.repo'
+touch /etc/yum.repos.d/mongodb.repo
+sh -c 'echo "[mongodb]" > /etc/yum.repos.d/mongodb.repo'
+sh -c 'echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb.repo'
+sh -c 'echo "baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/" >> /etc/yum.repos.d/mongodb.repo'
+sh -c 'echo "gpgcheck=0" >> /etc/yum.repos.d/mongodb.repo'
+sh -c 'echo "enabled=1" >> /etc/yum.repos.d/mongodb.repo'
+sh -c 'echo "" >> /etc/yum.repos.d/mongodb.repo'
 
 # Install epel repo
-sudo yum -y install epel-release
+yum -y install epel-release
 
 # Update repos
-sudo yum -y update
+yum -y update
 
 #install nodejs and npm
-sudo yum -y install nodejs npm
+yum -y install nodejs npm
 
 # Install mongodb
-sudo yum -y install mongodb-org mongodb-org-server
+yum -y install mongodb-org mongodb-org-server
 
 # Start mongodb as a service
-sudo systemctl start mongod
+systemctl start mongod
 
 # Install mean-cli
-sudo npm install -g gulp
-sudo npm install -g bower
-sudo npm install -g mean-cli
+echo "If not run manually the rights goes wrong here, not sure why."
+echo "Run the following commands"
+echo "sudo npm install -g gulp"
+echo "sudo npm install -g bower"
+echo "sudo npm install -g mean-cli"
 
